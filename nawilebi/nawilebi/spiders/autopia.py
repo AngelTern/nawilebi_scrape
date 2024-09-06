b0879a698b83dddd5c85a5bfa3577606a6e5bb09
AngelTern <georgepapava2@gmail.com>
2024-09-07 01:01:29 +0400

    Updated autopia.py: parse function ready, parse_car_mark and parse_car_part need to be finished

--- nawilebi/nawilebi/spiders/autopia.py
+++ nawilebi/nawilebi/spiders/autopia.py
@@ -7,4 +7,18 @@ class AutopiaSpider(scrapy.Spider):
     start_urls = ["https://autopia.ge"]
 
     def parse(self, response):
+        
+        car_marks = response.css("div.car_marks").get()
+        
+        for car_mark in car_marks:
+            relative_url = response.css("a ::attr(href)").get()
+            car_mark_url = "https://autopia.ge" + relative_url
+            
+            yield response.follow(car_mark_url, callback=self.parse_case_page)
+            
+    
+    def parse_car_mark(self, response):
+        pass
+    
+    def parse_car_part(self, response):
         pass
