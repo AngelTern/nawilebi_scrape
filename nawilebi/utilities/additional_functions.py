6bef03edd6ccf9355c6880ee2556ec990df8df73
AngelTern <georgepapava2@gmail.com>
2024-09-08 18:03:24 +0400

    added additional formating for car_model in pipelines

--- nawilebi/utilities/additional_functions.py
+++ nawilebi/utilities/additional_functions.py
@@ -3,7 +3,7 @@ import re
 def extract_numbers(string):
     return re.sub(r'\D', '', string)
 
-def process_part_full_name_autopia(part_full_name, year):
+def process_part_full_name_autopia(part_full_name, year, car_mark):
     
     if "სატესტო" in part_full_name:
         return None, None
@@ -16,7 +16,9 @@ def process_part_full_name_autopia(part_full_name, year):
     if "-" in modified_string:
         split_values = modified_string.split("-", 1)  
         part_name = split_values[0].strip()  
-        car_model = split_values[1].strip()  
+        car_model = split_values[1].strip()
+        car_model = car_model.replace(car_mark, '').strip()
+        
     else:
         part_name = modified_string 
         car_model = "" 
