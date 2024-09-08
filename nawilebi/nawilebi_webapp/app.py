d77a18d4f28e0fc455f760dd804d6eb501f59a52
AngelTern <georgepapava2@gmail.com>
2024-09-09 03:11:24 +0400

    finished inital version of flaskapp (no db loading yet) with necessary templates

--- nawilebi/nawilebi_webapp/app.py
+++ nawilebi/nawilebi_webapp/app.py
@@ -1,30 +1,65 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12XklsD!?NmG1509@localhost/nawilebi'
 app.config["SECRET_KEY"] = 'whatever'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     __tablename__ = 'login_credentials'
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), nullable = False)
+    username = db.Column(db.String(20), nullable = False, unique = True)
     password = db.Column(db.String(80), nullable = False)
 
-
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username", "class": "form-control"})
+    
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password", "class": "form-control"})
+    
+    submit = SubmitField("Login", render_kw={"class": "btn btn-primary"})
 
 @app.route('/')
 def home():
     return redirect(url_for('login'))
 
-@app.route('/login')
+@app.route('/login', methods = ["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for("dashboard"))
+    return render_template("login.html", form = form)
 
+@app.route('/dashboard', methods = ["GET", "POST"])
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
 
-
+@app.route('/logout', methods = ["GET", "POST"])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     with app.app_context():
