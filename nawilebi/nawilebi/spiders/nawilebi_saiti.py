7e11e2260fa65a38e1f887416dea595ed3fe276b
AngelTern <georgepapava2@gmail.com>
2024-09-21 22:14:48 +0400

    main spiders finished

--- nawilebi/nawilebi/spiders/nawilebi_saiti.py
+++ nawilebi/nawilebi/spiders/nawilebi_saiti.py
@@ -4,17 +4,10 @@ import scrapy
 class NawilebiSaitiSpider(scrapy.Spider):
     name = "nawilebi_saiti"
     allowed_domains = ["nawilebi.ge"]
-    start_urls = ["https://nawilebi.ge/ka"]
-
+    start_urls = ["https://nawilebi.ge/ka/parts"]
+    start_page = 1
     def parse(self, response):
-        car_mark_list = response.css("#page-container > div.container.m-b-20 > div > section > div > div")
         
-        for car_mark in car_mark_list:
-            relative_url = car_mark.css("div a::attr(href)").get()
-            car_mark_name = car_mark.css("div a::attr(title)").get().upper()
-            
-            yield response.follow(self.start_urls[0] + relative_url, callback = self.parse_model_page,
-                                  meta = {"car_mark": car_mark_name})
             
     def parse_model_page(self, response):
         pass
--- sdada.py
+++ sdada.py
@@ -1,12 +1,4 @@
-ascii_numbers = [
-    68, 92, 120, 48, 102, 92, 120, 49, 52, 70, 164, 170, 245, 92, 120, 57, 48, 34, 82, 92, 120, 57, 51, 122, 92, 120, 
-    48, 48, 212, 92, 120, 57, 49, 186, 240, 231, 207, 191, 191, 92, 120, 48, 56, 92, 120, 56, 99, 92, 120, 57, 98, 248, 
-    88, 231, 249, 254, 211, 105, 255, 95, 175, 92, 120, 57, 98, 74, 214, 92, 120, 57, 98, 193, 210, 174, 44, 176, 55, 
-    # ... continue with the rest of your numbers
-]
+import requests
 
-# Decode ASCII values to characters
-decoded_chars = [chr(num) if num < 128 else '?' for num in ascii_numbers]  # Replace non-ASCII with '?'
-decoded_text = ''.join(decoded_chars)
-
-print(decoded_text)
+r = requests.get("https://otoparts.ge/dzaris-natsilebi/fiat/")
+print(r)
